78c46c0b5b5bbd3bffa771667edbd7e6c06db86d
n_shimizu <n_shimizu@cns.s.u-tokyo.ac.jp>
2019-07-19 01:38:16 +0900

    [fix] misalignment of content when header is fixed #9

--- themes/cns_hp_theme/static/js/header.js
+++ themes/cns_hp_theme/static/js/header.js
@@ -20,10 +20,8 @@ $(function(){
     var value = $(this).scrollTop();
     if ( value > beltPos ) {
       $belt.addClass(fixedClass);
-      $main.css('margin-top', beltHeight);
     } else {
       $belt.removeClass(fixedClass);
-      $main.css('margin-top', '');
     }
     if ( value > beltPos/2 && value <= beltPos ) {
       var trans = (value-beltPos/2)/(beltPos-beltPos/2)*0.5+0.5;
