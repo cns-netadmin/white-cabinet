2724fcb48f8823edc85bb6c22b997b7ee098c850
Hideki Shimizu <shimiz@cns.s.u-tokyo.ac.jp>
2019-08-01 17:36:23 +0900

    [add] swiper, [mod] apply js to baseof.html, single.html, and change css of sidebar

--- static/js/container.js
+++ static/js/container.js
@@ -2,24 +2,48 @@ $(function(){
   var $win = $(window)
       $container = document.getElementsByClassName("container")[0],
       $topicList = document.getElementsByClassName("topic_list")[0],
+      $topics = document.getElementsByClassName("topic"),
       wideClass = 'container',
       narrowClass = 'container-narrow',
       value = 700, /* Size for Narrow version, Change here!*/
       oldBrowserWidth = value;
-      
+      console.log("start logging");
+    
+      function changeTopicsCSS(name){
+        for (var i=0; i<$topics.length; i++){
+            $topics[i].className = name;
+          }
+      }
+
+      var browserWidth = $win.width();
+      if( value < browserWidth ) { 
+         if ( value >= oldBrowserWidth ){
+           $container.className=wideClass;
+           $topicList.className="topic_list";
+           console.log("initial width: wide");
+         }
+      }else{
+         if (value <= oldBrowserWidth ){
+           $container.className=narrowClass;
+           $topicList.className="topic_list-narrow";
+           console.log("initial width: narrow");
+         }
+      }
+      oldBrowserWidth = browserWidth;
+
   $win.on('load resize', function() {
     var browserWidth = $win.width();
     if( value < browserWidth ) { 
        if ( value >= oldBrowserWidth ){
          $container.className=wideClass;
          $topicList.className="topic_list";
-        // console.log("hoge");
+         console.log("width changed: wide");
        }
     }else{
        if (value <= oldBrowserWidth ){
          $container.className=narrowClass;
          $topicList.className="topic_list-narrow";
-       //  console.log("fuga");
+         console.log("width changed: narrow");
        }
     }
     oldBrowserWidth = browserWidth;
