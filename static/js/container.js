5f0bbe385638311c31b51a65658428a9be884c65
Hideki Shimizu <shimiz@cns.s.u-tokyo.ac.jp>
2019-08-09 16:50:32 +0900

    [mod] adjust size, padding

--- static/js/container.js
+++ static/js/container.js
@@ -34,6 +34,7 @@ $(function(){
            $('.topic_list-narrow').not('.slick-initialized').slick({
              dots: true,
              respondTo: 'window',
+             centerPadding: '8px',
              infinite: true,
              slidesToShow: 1,
              autoplay: true,
